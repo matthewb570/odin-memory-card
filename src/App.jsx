219bac8c665df9db3f6912a868cc06db4763c534
Matthew Burgess <boog397@gmail.com>
2025-04-06 19:41:51 -0500

    Add ability to retrieve and display all Hyrule Compendium entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
 export default function App() {
-  return <div>Test</div>;
+  const [memoryCardData, setMemoryCardData] = useState([]);
+
+  useEffect(() => {
+    let active = true;
+
+    async function getData() {
+      const response = await fetch(
+        "https://botw-compendium.herokuapp.com/api/v3/compendium/all",
+      );
+      if (response.ok && active) {
+        const responseJson = await response.json();
+        setMemoryCardData(responseJson.data);
+      }
+    }
+
+    getData();
+
+    return () => {
+      active = false;
+    };
+  }, []);
+
+  return (
+    <>
+      {memoryCardData.map((entry) => (
+        <div>
+          <img src={entry.image}></img>
+          <div>{entry.name}</div>
+        </div>
+      ))}
+    </>
+  );
 }
