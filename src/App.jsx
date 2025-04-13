04ede59d5b9a540add5f3898f154067e7e2a6749
Matthew Burgess <boog397@gmail.com>
2025-04-12 22:31:25 -0500

    Move App.jsx and index.css to appropriate folders

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import "./styles/ImageCard.css";
-import "./styles/Game.css";
-import Game from "./components/Game";
-
-export default function App() {
-  return <Game />;
-}
---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import "../styles/ImageCard.css";
+import "../styles/Game.css";
+import Game from "./Game";
+
+export default function App() {
+  return <Game />;
+}
