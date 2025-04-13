f2eae92fad7ab47c1000b80f88de409e38fcaef4
Matthew Burgess <boog397@gmail.com>
2025-04-12 21:42:50 -0500

    Prevent image card clicks from registering if game is over and adjust image card font size

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -31,6 +31,10 @@ export default function Game() {
   }, []);
 
   function handleMemoryCardClick(index) {
+    if (isGameOver) {
+      return;
+    }
+
     if (memoryCardsToDisplay[index].isClicked) {
       endGame();
     } else {
